93cfbf4344a9b81803cb0fbd3bbc37d19236f85a
root <tudorb_2007@yahoo.com>
2017-07-29 12:12:36 +0300

    Basic form modified. Verified validation in views. Added upload file. zip,rar filter.

--- app/forms.py
+++ app/forms.py
@@ -3,12 +3,16 @@
 Python Aplication Template
 Licence: GPLv3
 """
-from flask_wtf          import Form
-from wtforms            import StringField, TextAreaField, SubmitField
-from wtforms.validators import InputRequired, Email, DataRequired
+from flask_wtf          	 import Form
+from flask_wtf.file          import FileRequired
+from wtforms            	 import StringField, TextAreaField, SubmitField, FileField
+from wtforms.validators		 import InputRequired, Email, DataRequired
 
 class NewAssignmentForm(Form):
-	title       = StringField(u'Title'			, validators=[DataRequired()])
-	info        = StringField(u'Info'			, validators=[DataRequired()])
-	description = StringField(u'Description'	, validators=[DataRequired()])
-	tags        = StringField(u'Tags'			, validators=[DataRequired()])
+	title       = StringField('Title'			, [DataRequired(), InputRequired()])
+	info        = StringField('Info'			, [DataRequired(), InputRequired()])
+	description = StringField('Description'		, [DataRequired(), InputRequired()])
+	tags        = StringField('Tags'			, [DataRequired(), InputRequired()])
+	file        = FileField  ('Assignment'		, [FileRequired()])#, Regexp('(\.{1}(zip|rar))$', message='File must be zip or rar')])
+
+	
--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ Python Aplication Template
 Licence: GPLv3
 """
 
+import re, os
 from flask import url_for, redirect, render_template, flash, g, session, jsonify, request
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app
@@ -21,21 +22,32 @@ def index():
 @app.route('/add_assignment', methods=['GET', 'POST'])
 def add_assignment():
 
+    form = NewAssignmentForm()
+
     submit = request.form.get('submit', None, type=str)
 
     # POST ..
     if submit:
-
+        if not form.validate():
+            return 'Submission failed'
         title = request.form.get('title',        '', type=str)
         info  = request.form.get('info',         '', type=str)
         desc  = request.form.get('description',  '', type=str)
         tags  = request.form.get('tags',         '', type=str)
+        file  = form.file.data
 
+        if not re.match("(.+\.{1}(zip|rar)$)", str(file.filename)):
+            return 'File type unsupported'
+        #else:
+        #    return 'OK'
+        file.save(os.path.join(os.getcwd(), 'uploads', file.filename))
+        #file.save(os.path.join('/home', 'ginfuu', 'uploads', file.filename))
         obj      = Object()
         obj.key1 = title ;
         obj.key2 = info  ;
         obj.key3 = desc  ;
         obj.key4 = tags  ;
+        obj.key5 = file.filename;
 
         db.session.add   ( obj ); # open transaction 
         db.session.commit(     ); # save
@@ -45,7 +57,7 @@ def add_assignment():
     # On GET we display the form ..         
     else:    
         return render_template('common/add_assignment.html',
-                                form=NewAssignmentForm())
+                                form=form)
 
 # View assignment
 @app.route('/view_assignment', methods=['GET'])
